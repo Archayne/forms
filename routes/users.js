b753a7bdc25bca0eed0c66d08b341130bf5f6a07
Archayne <hargrovea31@gmail.com>
2026-03-27 20:49:59 -0400

    added lastname, gender, and age to the /new route

--- routes/users.js
+++ routes/users.js
@@ -6,21 +6,33 @@ router.route('/').get((req, res)=>{
     res.send('User List');
 }).post((req, res)=>{
     const firstName = req.body.firstName;
-    const isValid = firstName !=="";
-    if(isValid){
-        console.log(`Adding user: ${firstName}`);
-        users.push({firstName});
+    const lastName = req.body.lastName;
+    const gender = req.body.gender;
+    const age = req.body.age;
+    const isValidFirstName = firstName !=="";
+    const isValidLastName = lastName !=="";
+    const isValidGender = gender !=="";
+    const isValidAge = age !=="";
+    if(isValidFirstName && isValidLastName && isValidGender && isValidAge){
+        console.log(`Adding user: ${firstName} - ${lastName} - ${gender} - ${age}`);
+        console.log(`Adding user: ${age}`);
+        users.push({firstName, lastName, gender, age});
         res.render('users/list', {users});
     }else{
         console.log("Error adding user!");
-        res.render("users/new", {firstName:firstName});
+        res.render("users/new", {firstName:firstName, lastName:lastName, gender:gender, age:age});
     }
 });
 router.get('/list', (req, res)=>{
 res.render('users/list', {users});
 });
 router.get('/new', (req, res)=>{ // /users/new
-res.render('users/new', {firstName:"Test"});
+    res.render('users/new', {
+        firstName:"Enter first Name",
+        lastName:"Enter last Name",
+        gender:"Enter gender",
+        age:"Enter age"
+    });
 });
 /*
 router.get('/:id', (req, res)=>{
@@ -29,7 +41,7 @@ router.get('/:id', (req, res)=>{
 */
 router.route('/:id').get((req, res)=>{
     console.log(req.user);
-    console.log("Gettingg user data!");
+    console.log("Getting user data!");
     res.send(`getting User Data for id: ${req.user}`);
 }).delete((req, res)=>{
     res.send(`Deleting User Data for id: ${req.params.id}`);
@@ -37,7 +49,8 @@ router.route('/:id').get((req, res)=>{
     res.send(`Updating User Data for id: ${req.params.id}`);
 });
 
-const users = [{name:"George"}, {name:"Justyna"}];
+//const users = [{name:"George"}, {name:"Justyna"}];
+const users = [{firstName, lastName, gender, age}];
 router.param("id", (req, res, next, id)=>{
     console.log("Access attmept by User:", id);
     req.user = users[id];
